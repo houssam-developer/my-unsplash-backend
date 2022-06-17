1fd5be1c9f8513282795f5f42c24f4f8480d4da6
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-17 14:58:13 +0100

    add cors for local dev

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const startup = () => {
 	const app = express();
+
+	app.use(cors());
 
 	app.get('/', (req, res) => {
 		return res.send('Hello World!');
